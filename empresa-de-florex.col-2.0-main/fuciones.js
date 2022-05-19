1b8438de7ca4a5937518427d10cb6ae30a72eb72
RuizJohan4 <100625072+RuizJohan4@users.noreply.github.com>
2022-05-19 11:16:57 -0500

    Add files via upload

---
+++ empresa-de-florex.col-2.0-main/fuciones.js
@@ -0,0 +1,11 @@
+function datosclientes() {
+    var nombre = document.getElementById("exampleInputEmail1");
+    var password = document.getElementById("exampleInputPassword1");
+
+    alert(nombre.value + "\n" + password.value);
+
+
+
+
+
+}
